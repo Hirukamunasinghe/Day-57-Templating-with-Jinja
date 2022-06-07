173ce31e24a55cdd1ffd5810d4113e988d9207ba
Hirukamunasinghe <munasinghehiruka@gmail.com>
2022-06-07 11:21:06 +0530

    Initial commit

--- server.py
+++ server.py
@@ -14,25 +14,24 @@ def home():
 @app.route("/guess/<name>")
 def guess(name):
     gender_url =f"https://api.genderize.io?name={name}"
-    gender_response = requests.get(gender_url)
-    gender_data = gender_response.json()
-    gender = gender_data["gender"]
+    gender_data = requests.get(gender_url).json()
+    gender = gender_data['gender']
 
-    age_url= f"https://api.agify.io?name={name}"
-    age_response = requests.get(age_url)
-    age_data = age_response.json()
-    age = age_data["age"]
-    return render_template("guess.html",person_name =name,gender=gender,age=age)
+    age_url = f"https://api.agify.io?name={name}"
+    age_data = requests.get(age_url).json()
+    age = age_data['age']
+
+    return render_template("guess.html",person_name=name,age=age,gender=gender)
 
 
 @app.route("/blog/<num>")
 def get_blog(num):
     print(num)
     blog_url= "https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(blog_url)
-    all_posts = response.json()
+    all_posts = requests.get(blog_url).json()
     return render_template("blog.html",posts=all_posts)
 
 
 if __name__ =="__main__":
     app.run(debug=True)
+
